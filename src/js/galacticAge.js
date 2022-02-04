e77c3298985432dd8049069bae5e7a9ee4996bf0
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-04 09:22:41 -0800

    commit after good pass  of spec-2

--- src/js/galacticAge.js
+++ src/js/galacticAge.js
@@ -3,6 +3,8 @@ export default class GalacticAge{
     this.age = age;
   }
   AgeInMercuryYears(){
-    
+    let mercuryAge=Math.floor(this.age/.24);
+    console.log(mercuryAge);
+    return mercuryAge;
   }
 }
