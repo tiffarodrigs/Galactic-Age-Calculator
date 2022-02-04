12968758e419fcf9c938814e99bbbcd6d3fc1ee7
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-04 08:23:35 -0800

    Change title in json and config file

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(), // new line
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Galactic-Age-Calculator',
       template: './src/index.html',
       inject: 'body'
     })
