dcb8e2a58018f3bc2e3cc3e7f0cc5aa7a40a7670
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-04 08:48:22 -0800

    commit after good fail-red

---
+++ _tests_/galacticAge.test.js
@@ -0,0 +1,10 @@
+import GalacticAge from './../src/js/galacticAge.js';
+
+describe('GalacticAge', () => {
+
+  test('should create a GalacticAge object with the given age', () => {
+    const galacticAge = new GalacticAge(24);
+    expect(galacticAge.age).toEqual(24);
+
+  });
+});
---
+++ src/js/galacticAge.js
@@ -0,0 +1,7 @@
+export default class GalacticAge{
+  constructor(age){
+    
+  }
+
+
+}
