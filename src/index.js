22889eca9765fd51ab375d98951d88de8f0ead7d
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-04 16:17:55 -0800

    WIP-add code in UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-//import files
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import GalacticAge from "./js/galacticAge";
+
+$("#Mercury").click(() =>{
+
+});
